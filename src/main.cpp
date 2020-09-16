b5e7dbb7790ef60196c58621a6f7194bbff56e3a
Alexej Harm <alexej.h@xiphos.de>
2020-09-16 10:33:58 +0200

    empty main

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,4 @@
-#include "c.hpp"
-#include <iostream>
-
 int main(int argc, char* argv[])
 {
-  std::cout << "a: " << a() << std::endl;
-  std::cout << "b: " << b() << std::endl;
-
-  std::cout << "inline a: " << inline_a() << std::endl;
-  std::cout << "inline b: " << inline_b() << std::endl;
+  return EXIT_SUCCESS;
 }
